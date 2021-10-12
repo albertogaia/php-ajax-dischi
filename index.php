2ae62d60e34d97bbd84834c939282f26ae5e9962
albertogaia <alberto.gaia95@gmail.com>
2021-10-12 14:32:18 +0200

    Creazione card statica

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 include __DIR__ . '/utilities/functions.php';
+include __DIR__ . '/utilities/database.php';
 ?>
 
 <!DOCTYPE html>
@@ -8,10 +9,32 @@ include __DIR__ . '/utilities/functions.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Ajax Dischi</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;300;400;700&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="css/style.css">
+    <title>PHP Ajax Dischi</title>
 </head>
 <body>
-    
+    <header class="header">
+        <div class="logo-img"></div>
+        <div class="selection"></div>
+    </header>
+    <main class="main">
+        <div class="container">
+            <div class="card">
+                <div class="card__img">
+                    <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+                </div>
+                <div class="card__info">
+                    <div class="card__title"><h3>New Jersey</h3></div>
+                    <div class="card__author">
+                        <h4>Bon Jovi</h4>
+                        <h4>1988</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
