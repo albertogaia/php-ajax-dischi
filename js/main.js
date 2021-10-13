469a0ce841b658ad99811631f90f7bea3c2a8a7f
albertogaia <alberto.gaia95@gmail.com>
2021-10-13 12:00:49 +0200

    Lista filtrata

--- js/main.js
+++ js/main.js
@@ -5,15 +5,38 @@ const app = new Vue({
     data: {
         url: 'http://localhost:8888/ESERCITAZIONI/php-ajax-dischi/utilities/API/server.php',
         songs: [],
+        genres:[],
+        genreSelected: '',
+        filteredGenre: [],
     },
     methods: {
-        
+        getFilteredList(){
+            if(this.genreSelected == 'all'){
+                this.filteredGenre = this.songs;
+            }else{
+                this.filteredGenre = this.songs.filter(song=>{
+                    if(song.genre == this.genreSelected){
+                        return song;
+                    }
+                })
+            }
+        },
     },
     created() {
         axios
             .get(this.url)
             .then(res=>{
                 this.songs = res.data;
+                for(let i = 0; i < res.data.length; i++){
+                    if(!this.genres.includes(res.data[i].genre)){
+                    this.genres.push(res.data[i].genre)
+                    }
+                }
+                this.getFilteredList()
             })
+          
+    },
+    mounted() {
+        this.genreSelected = 'all';
     },
 })
