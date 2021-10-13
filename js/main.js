ea61e114a2f8cd482b0f239577ce4e177e7c171d
albertogaia <alberto.gaia95@gmail.com>
2021-10-13 12:31:52 +0200

    Dark-mode & filtered selection with local storage

--- js/main.js
+++ js/main.js
@@ -6,21 +6,38 @@ const app = new Vue({
         url: 'http://localhost:8888/ESERCITAZIONI/php-ajax-dischi/utilities/API/server.php',
         songs: [],
         genres:[],
-        genreSelected: '',
+        genreSelected: localStorage.getItem('genre-selected') || '',
         filteredGenre: [],
+        currentTheme: localStorage.getItem('theme-color') || 'dark-theme',
     },
     methods: {
         getFilteredList(){
+            const storedGenre = localStorage.getItem('genre-selected');
             if(this.genreSelected == 'all'){
                 this.filteredGenre = this.songs;
+                localStorage.setItem('genre-selected', 'all');
+                this.genreSelected = localStorage.getItem('genre-selected');
             }else{
                 this.filteredGenre = this.songs.filter(song=>{
                     if(song.genre == this.genreSelected){
                         return song;
                     }
+                    localStorage.setItem('genre-selected', this.genreSelected);
+                    this.genreSelected = localStorage.getItem('genre-selected');
                 })
             }
         },
+
+        changeTheme(){
+            const storedTheme = localStorage.getItem('theme-color');
+            if(storedTheme === 'dark-theme'){
+                localStorage.setItem('theme-color', 'light-theme');
+                this.currentTheme = localStorage.getItem('theme-color');
+            }else{
+                localStorage.setItem('theme-color', 'dark-theme');
+                this.currentTheme = localStorage.getItem('theme-color');
+            }
+        }
     },
     created() {
         axios
@@ -28,15 +45,13 @@ const app = new Vue({
             .then(res=>{
                 this.songs = res.data;
                 for(let i = 0; i < res.data.length; i++){
-                    if(!this.genres.includes(res.data[i].genre)){
-                    this.genres.push(res.data[i].genre)
-                    }
+                    !this.genres.includes(res.data[i].genre) ? this.genres.push(res.data[i].genre) : null
+                    
                 }
                 this.getFilteredList()
             })
           
     },
     mounted() {
-        this.genreSelected = 'all';
     },
 })
